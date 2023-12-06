746d01306edb8628dfb099e7bf294927e771eb69
berinaspirjan <berina.spirjan@treca-gimnazija.edu.ba>
2023-12-06 01:25:14 +0100

    Installed necessary dependencies, setup webcam and canvas, defined references for canvas and webcam, added functions for loading facemesh and detecting face

--- src/App.js
+++ src/App.js
@@ -2,9 +2,57 @@ import React, { useRef } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as facemash from "@tensorflow-models/facemesh";
 import Webcam from "react-webcam";
+import useStyles from "./styles";
 
 function App() {
-  return <div className="App"></div>;
+  const classes = useStyles();
+
+  const webCamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  async function runFacemesh() {
+    const net = await facemash.load({
+      inputResolution: {
+        width: "640px",
+        height: "480px",
+      },
+      scale: 0.8,
+    });
+
+    setInterval(() => {
+      detectFace(net);
+    }, 100);
+  }
+
+  async function detectFace(net) {
+    if (
+      typeof webCamRef.current !== "undefined" &&
+      webCamRef.current !== null &&
+      webCamRef.current.video.readyState === 4
+    ) {
+      const video = webCamRef.current.video;
+      const videoWidth = video.videoWidth;
+      const videoHeight = video.videoHeight;
+
+      video.width = videoWidth;
+      video.height = videoHeight;
+
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      const face = await net.estimateFaces(video);
+    }
+  }
+
+  runFacemesh();
+  return (
+    <div className={classes.header}>
+      <header>
+        <Webcam ref={webCamRef} className={classes.container} />
+        <canvas ref={canvasRef} className={classes.container} />
+      </header>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,17 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  container: {
+    position: "absolute",
+    marginLeft: "auto",
+    marginRight: "auto",
+    left: 0,
+    right: 0,
+    textAlign: "center",
+    zIndex: 9,
+    width: "640px",
+    height: "480px",
+  },
+});
+
+export default useStyles;
